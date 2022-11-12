bf627cd7765b860a7a0bf900cd443a88ef4b529a
noahdraper <noahdraper@noahs-mbp-3.lan>
2022-11-12 18:46:27 -0500

    NASA image api

--- MainPackage/Main.py
+++ MainPackage/Main.py
@@ -8,16 +8,26 @@ import json
 import requests
 import webbrowser
 
-def nasapic():
+#Show NASA Astronomy pic of the day
+def APOD():
 
     #data request
     params = {"api_key": "6mg0rSabfpZ3xx3RujevZPu8wqlxgGa1AcWjxbno", "hd": True, "count" : 1}    
     API = r"https://api.nasa.gov/planetary/apod?"
     data = requests.get(API, params = params)
     
-    title = json.loads(data.text) #parsed json - title of the image
+    parsed_json = json.loads(data.text) #python dictionary
+
     
-    print(title[0]["title"])
-    webbrowser.open(title[0]["url"]) #shows image in browser window
-nasapic()
+    print("Title:", parsed_json[0]["title"]) #title of photo
+    print("Explanation:", parsed_json[0]["explanation"]) #Explanation of photo
+    print("Date:", parsed_json[0]["date"]) #Date Taken
+    
+    if "copyright" in parsed_json[0]:
+        print("Copyright:", parsed_json[0]["copyright"]) #Copyright owner
+    else:
+        print("Copyright not listed")
+    
+    webbrowser.open(parsed_json[0]["url"]) #shows image in browser window
+APOD()
 
